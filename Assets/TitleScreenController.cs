3622bd7c3988a1be4a1926a55915ec9daaaa82e0
marco <marcojkuang@gmail.com>
2016-11-07 19:48:15 -0800

    fixed a bug of UI arrangements

---
+++ Assets/TitleScreenController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour {
+
+	public void LoadScene(string sceneName){
+
+		SceneManager.LoadScene (sceneName);
+
+	}
+
+	public void QuitGame(){
+
+		Application.Quit ();
+
+	}
+
+
+/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+*/
+}
